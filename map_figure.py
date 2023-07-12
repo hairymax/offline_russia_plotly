36e9da3091fed7e72f126059d5785c91832f334d
hairymax <aleshin.m.s@gmail.com>
2023-07-12 17:23:40 +1000

    Example notebook, mapFigure module

--- map_figure.py
+++ map_figure.py
@@ -1,33 +1,43 @@
+'''Класс для слоя подложки карты России'''
+
 import pandas as pd
+import geopandas as gpd
 import plotly.graph_objects as go
-import plotly.express as px
+from shapely.geometry import Point
+
+REGIONS = pd.read_parquet("data/russia_regions.parquet")
 
 
-fo_list = list(regions['federal_district'].unique())
+def convert_crs(x_arr, y_arr, to_crs='EPSG:32646', from_crs="EPSG:4326"):
+    """Преобразование значений координат в массивах x_arr и y_arr
+    из географической системы отсчёта from_crs в систему to_crs
+    """
+    data = [Point(x,y) for x,y in zip(x_arr, y_arr)]
+    pts = gpd.GeoSeries(data, crs=from_crs).to_crs(to_crs)
+    
+    return pts.x, pts.y
+
 
 class mapFigure(go.Figure):
     """ Шаблон фигуры для рисования поверх карты России
     """
     def __init__(self, 
-        data=None, layout=None, frames=None, skip_invalid=False, # дефолтные параметры фигуры plotly
+        data=None, layout=None, frames=None, skip_invalid=False, # дефолтные параметры plotly
         **kwargs # аргументы (см. документацию к plotly.graph_objects.Figure())
     ):
         # создаём plotlу фигуру с дефолтными параметрами
         super().__init__(data, layout, frames, skip_invalid, **kwargs)
-        
-        # палитра цветов
-        colors = px.colors.qualitative.Plotly
 
         # прорисовка регионов
-        for i, r in regions.iterrows():
+        for i, r in REGIONS.iterrows():
             self.add_trace(go.Scatter(x=r.x, y=r.y,
                                       name=r.region,
-                                      text=f'<b>{r.region}</b><br>{r.federal_district}<br>Население: {r.population}',
+                                      text=r.region,
                                       hoverinfo="text",
                                       line_color='grey',
                                       fill='toself',
-                                      line_width=2,
-                                      fillcolor=colors[fo_list.index(r.federal_district)],
+                                      line_width=1,
+                                      fillcolor='lightblue',
                                       showlegend=False
             ))
         
@@ -35,7 +45,7 @@ class mapFigure(go.Figure):
         self.update_xaxes(visible=False)
         self.update_yaxes(visible=False, scaleanchor="x", scaleratio=1)
 
-        # чтобы покрасивее вписывалась карта на поверхность фигуры, значения можно будет переопределять
+        # чтобы покрасивее вписывалась карта на поверхности фигуры
         self.update_layout(showlegend=False, dragmode='pan',
-                           width=800, height=450, 
-                           margin={'l': 10, 'b': 10, 't': 10, 'r': 10})  # отступы по умолчанию слишком большие
+                           width=1000, height=600, 
+                           margin={'l': 10, 'b': 10, 't': 10, 'r': 10}) # отступы
